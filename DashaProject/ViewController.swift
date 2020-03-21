0e940b8adf84a8c4b69095f77a0ccef03bbeb861
George <georgii.kashin@gmail.com>
2020-03-21 12:44:43 +0300

    Initial Commit

---
+++ DashaProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DashaProject
+//
+//  Created by Георгий Кашин on 21.03.2020.
+//  Copyright © 2020 Georgii Kashin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
